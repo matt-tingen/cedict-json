090412187601d0c626f09bf11d53dbb7cfd98455
Matt Tingen <matt.tingen@gmail.com>
2025-03-22 20:41:50 -0400

    Setup GHA

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { writeFile } from 'node:fs/promises';
 import { promisify } from 'node:util';
 import { gunzip as gunzipCb } from 'node:zlib';
+import pkg from '../package.json';
 import { CedictParser } from './CedictParser';
 import { download } from './download';
 
@@ -20,13 +21,14 @@ export const main = async (sourceUrl: string, destinationPath: string) => {
   if (parsed.metadata.get('license') !== EXPECTED_LICENSE)
     throw new Error(`Unexpected license: ${license}`);
 
+  await writeFile(destinationPath, JSON.stringify(parsed.entries));
+
   const dictionaryDate = parsed.metadata.get('date');
   if (!dictionaryDate) throw new Error('Missing dictionary timestamp');
 
-  const version = dictionaryDate.split('T')[0].replaceAll(/[^\d]/g, '');
+  const timestamp = dictionaryDate.split('T')[0].replaceAll(/[^\d]/g, '');
+  const version = pkg.version.replace(/\.\d+$/, '.' + timestamp);
 
   // eslint-disable-next-line no-console
-  console.log({ version });
-
-  await writeFile(destinationPath, JSON.stringify(parsed.entries));
+  console.log(version);
 };
